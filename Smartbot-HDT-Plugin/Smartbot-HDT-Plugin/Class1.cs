e5a8d12079a53654a7b46ab08bbb48cb7c00fb6a
KryTonX <andrasmumm99@yahoo.de>
2018-01-04 23:16:23 +0100

    V0.0.1
    Empty project

---
+++ Smartbot-HDT-Plugin/Smartbot-HDT-Plugin/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Smartbot_HDT_Plugin
+{
+    public class Class1
+    {
+    }
+}
